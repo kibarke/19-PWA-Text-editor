4e28c70d3a1f8526f9d20fc205eb022fcfbc0496
kibarke <kibarkework@gmail.com>
2024-05-22 11:34:03 -0400

    added asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,9 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+                    request.destination === 'style' ||
+                    request.destination === 'image',
+  assetCache
+);
